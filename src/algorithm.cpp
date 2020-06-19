428f98a8a47257cc86632871faff4ca8614217e9
dave.lin@azurewave.com <dave.lin@azurewave.com>
2020-06-19 16:59:52 +0800

    Matrix mutiply , determinant , Cofactor , adjoint , inverse , transpose

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -87,3 +87,230 @@ void Fibonacci(std::vector<int> vec_int, int& value_max, int& posi_max) {
 		before_sharpness = vec_int.at(posi);
 	}
 }
+
+//矩陣乘法 https://docs.microsoft.com/zh-tw/cpp/parallel/amp/walkthrough-matrix-multiplication?view=vs-2019
+bool MutipleWithoutAMP(std::vector<std::vector<float>> aMatrix, std::vector<std::vector<float>> bMatrix, std::vector<std::vector<float>>& product)
+{
+	int inner_max = aMatrix.at(0).size();
+	if (inner_max != bMatrix.size())
+		return false;
+
+	int row_max = aMatrix.size();
+	int col_max = bMatrix.at(0).size();
+
+	product = std::vector<std::vector<float>>(row_max, std::vector<float>(col_max, 0));
+
+	for (int row = 0; row < row_max; row++) {
+		for (int col = 0; col < col_max; col++) {
+			// Multiply the row of A by the column of B to get the row, column of product.
+			for (int inner = 0; inner < inner_max; inner++) {
+				product[row][col] += aMatrix[row][inner] * bMatrix[inner][col];
+			}
+		}
+	}
+
+	return true;
+}
+
+#include <amp.h>
+bool MutipleWithAMP(std::vector<std::vector<float>> aMatrix, std::vector<std::vector<float>> bMatrix, std::vector<std::vector<float>>& product)
+{
+	int inner_max = aMatrix.at(0).size();
+	if (inner_max != bMatrix.size())
+		return false;
+
+	int row_max = aMatrix.size();
+	int col_max = bMatrix.at(0).size();
+
+	//2d vector -> 1d vector
+	std::vector<float> a_1d;
+	for (auto tmp : aMatrix)
+		for (auto tmp_int : tmp)
+			a_1d.push_back(tmp_int);
+
+	std::vector<float> b_1d;
+	for (auto tmp : bMatrix)
+		for (auto tmp_int : tmp)
+			b_1d.push_back(tmp_int);
+
+	std::vector<float> product_1d = std::vector<float>(row_max * col_max, 0);
+
+	Concurrency::array_view<float, 2> a(row_max, inner_max, a_1d);
+	Concurrency::array_view<float, 2> b(inner_max, col_max, b_1d);
+	Concurrency::array_view<float, 2> productMatrix(row_max, col_max, product_1d);
+
+	Concurrency::parallel_for_each(productMatrix.extent,
+		[=](concurrency::index<2> idx) restrict(amp) {
+			int row = idx[0];
+			int col = idx[1];
+			for (int inner = 0; inner < inner_max; inner++) {
+				productMatrix[idx] += a(row, inner) * b(inner, col);
+			}
+		});
+
+	productMatrix.synchronize();
+
+	//1d vector -> 2d vector
+	for (int i = 0; i < row_max; i++) {
+		std::vector<float> tmp;
+		for (int j = 0; j < col_max; j++) {
+			tmp.push_back(product_1d[i * col_max + j]);
+		}
+		product.push_back(tmp);
+	}
+
+	return true;
+}
+
+//行列式 determinant
+// Function to get cofactor of mat[p][q] in temp[][]. n is current 
+// dimension of mat[][] 
+void getCofactor(std::vector<std::vector<float>> mat, std::vector<std::vector<float>>& temp, int p, int q, int n)
+{
+	int i = 0, j = 0;
+
+	// Looping for each element of the matrix 
+	for (int row = 0; row < n; row++)
+	{
+		for (int col = 0; col < n; col++)
+		{
+			//  Copying into temporary matrix only those element 
+			//  which are not in given row and column 
+			if (row != p && col != q)
+			{
+				temp[i][j++] = mat[row][col];
+
+				// Row is filled, so increase row index and 
+				// reset col index 
+				if (j == n - 1)
+				{
+					j = 0;
+					i++;
+				}
+			}
+		}
+	}
+}
+
+//matrix is NxN matrix
+float determinantOfMatrix(std::vector<std::vector<float>> matrix, int n)
+{
+	float D = 0; // Initialize result 
+
+	//  Base case : if matrix contains single element 
+	if (n == 1)
+		return matrix[0][0];
+
+	std::vector<std::vector<float>> temp(n, std::vector<float>(n, 0)); // To store cofactors 
+
+	int sign = 1;  // To store sign multiplier 
+
+	 // Iterate for each element of first row 
+	for (int f = 0; f < n; f++)
+	{
+		// Getting Cofactor of mat[0][f] 
+		getCofactor(matrix, temp, 0, f, n);
+		D += sign * matrix[0][f] * determinantOfMatrix(temp, n - 1);
+
+		// terms are to be added with alternate sign 
+		sign = -sign;
+	}
+
+	return D;
+}
+
+//伴隨矩陣
+//because irrational number can't save by float
+//so it will exists deviation
+void ADJ_matrix(std::vector<std::vector<float>> matrix, int n, std::vector<std::vector<float>>& adj_matrix)//to find adjoint matrix
+{
+	adj_matrix = std::vector<std::vector<float>>(n, std::vector<float>(n, 0));
+	if (n == 1) {
+		adj_matrix[0][0] = 1; return;
+	}
+
+	int s = 1;
+	std::vector<std::vector<float>> temp(n, std::vector<float>(n, 0)); // To store cofactors 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			//To get cofactor of M[i][j]
+			getCofactor(matrix, temp, i, j, n);
+			s = ((i + j) % 2 == 0) ? 1 : -1; //sign of adj[j][i] positive if sum of row and column indexes is even.
+			adj_matrix[j][i] = (s) * (determinantOfMatrix(temp, n - 1)); //Interchange rows and columns to get the transpose of the cofactor matrix
+		}
+	}
+}
+
+//反矩陣，Inverse Matrix
+//because irrational number can't save by float
+//so it will exists deviation
+bool Inverse_matrix(std::vector<std::vector<float>>matrix, std::vector<std::vector<float>>& inv_matrix)
+{
+	int n = matrix.size();
+	float det = determinantOfMatrix(matrix, n);
+	if (det == 0)
+		return false;
+
+	std::vector<std::vector<float>>adj_matrix;
+	ADJ_matrix(matrix, matrix.size(), adj_matrix);
+
+	inv_matrix = std::vector<std::vector<float>>(n, std::vector<float>(n, 0));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			inv_matrix[i][j] = adj_matrix[i][j] / det;
+
+	return true;
+}
+
+//Transpose matrix，轉置矩陣
+std::vector<std::vector<float>> transpose_matrix(std::vector<std::vector<float>>matrix) 
+{
+	int row = matrix.size();
+	if (row < 1)
+		return;
+
+	int col = matrix.at(0).size();
+	std::vector<std::vector<float>> transpose_mat = std::vector<std::vector<float>>(col, std::vector<float>(row, 0));
+
+	for (int i = 0; i < row; i++)
+		for (int j = 0; j < col; j++)
+			transpose_mat[j][i] = matrix[i][j];
+
+	return transpose_mat;
+}
+
+//typedef std::pair<int, int> Double_int;
+//離散差分方程 https://blog.csdn.net/qq_24548569/article/details/85838201
+//void DDEPM(std::vector<int> vec_sharpness) {
+//	if (vec_sharpness.size() < 3) {
+//		std::cout << "the length of sequence is less than 3" << std::endl;
+//		return;
+//	}
+//
+//	//累加生成運算 Accumulated Generating Operation
+//	std::vector<int> vec_AGO;
+//	int xi = 0;
+//	for (auto i : vec_sharpness) {
+//		xi += i;
+//		vec_AGO.push_back(xi);
+//	}
+//	
+//	//DDE(2,1) 二接離散差分方程式
+//	std::vector<Double_int> X;
+//	std::vector<int> Y;
+//
+//	for (int i = 0; i < vec_AGO.size() - 2; i++) {
+//		X.push_back(Double_int(vec_AGO.at(i), vec_AGO.at(i + 1)));
+//		Y.push_back(vec_AGO.at(i + 2));
+//	}
+//		for i in range(len(self.x1) - 2) :
+//			X_row = [-self.x1[i + 1], -self.x1[i]]
+//			X.append(X_row)
+//			Y.append(self.x1[i + 2])
+//			X = np.array(X)
+//			Y = np.array(Y).reshape(-1, 1)
+//
+//			phi = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(Y)
+//			self.a = phi[0, 0]
+//			self.b = phi[1, 0]
+//}
